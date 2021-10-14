30da33ffb1369003ff29aeec73ae66cd8fe80284
kleberbarilli <kleberbarilli73@gmail.com>
2021-10-14 12:22:07 -0300

    setup inicial

---
+++ src/shared/server.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(process.env.PORT,() => {
+    console.log(`Rodando na porta ${process.env.PORT}`);
+})
