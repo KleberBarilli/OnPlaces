d3f093ec5fffba4e5cd191b9fdf8494b15450ec5
kleberbarilli <kleberbarilli73@gmail.com>
2021-10-14 16:38:11 -0300

    criacao da classe de errors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,12 +1,29 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import '@shared/infra/typeorm';
+import '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+	if (error instanceof AppError) {
+		return res.status(error.statusCode).json({
+			status: 'error',
+			message: error.message,
+		});
+	}
+	return res.status(500).json({
+		status: 'error',
+		message: error.message,
+	});
+});
 
 app.listen(process.env.PORT, () => {
 	console.log(`Rodando na porta ${process.env.PORT}`);
