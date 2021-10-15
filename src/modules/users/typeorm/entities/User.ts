18fbfab3223fd2d54f33c6c268106bbc8ced7fb5
kleberbarilli <kleberbarilli73@gmail.com>
2021-10-15 16:14:54 -0300

    user entities

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,42 @@
+import {
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	UpdateDateColumn,
+	CreateDateColumn,
+} from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
+import { IUser } from '@modules/users/domain/models/IUser';
+
+@Entity('users')
+export default class User implements IUser {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column()
+	name: string;
+
+	@Column()
+	email: string;
+
+	@Column()
+	@Exclude()
+	password: string;
+
+	@Column()
+	avatar: string;
+
+	@CreateDateColumn()
+	created_at: Date;
+
+	@UpdateDateColumn()
+	updated_at: Date;
+
+	@Expose({ name: 'avatar_url' })
+	getAvatarUrl(): string | null {
+		if (!this.avatar) {
+			return null;
+		}
+		return `${process.env.APP_API_URL}/files/${this.avatar}`;
+	}
+}
