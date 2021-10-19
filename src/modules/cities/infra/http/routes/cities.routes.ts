1658dec73dbf4c5c0679bbf51234262f92988761
kleberbarilli <kleberbarilli73@gmail.com>
2021-10-19 14:39:19 -0300

    city routes

---
+++ src/modules/cities/infra/http/routes/cities.routes.ts
@@ -0,0 +1,84 @@
+import { Router } from 'express';
+import CitiesController from '../controllers/CitiesController';
+import { celebrate, Joi, Segments } from 'celebrate';
+import isAuthenticated from '@shared/infra/http/middlewares/isAuthenticated';
+
+const citiesRouter = Router();
+const citiesController = new CitiesController();
+
+citiesRouter.get('/', citiesController.index);
+
+citiesRouter.get(
+	'/user/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
+	citiesController.index_user_cities,
+);
+
+citiesRouter.use(isAuthenticated);
+
+citiesRouter.get(
+	'/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
+	citiesController.show,
+);
+
+citiesRouter.post(
+	'/',
+	celebrate({
+		[Segments.BODY]: {
+			name: Joi.string().required(),
+			state: Joi.string(),
+			country: Joi.string().required(),
+			population: Joi.number().required(),
+			latitude: Joi.number().required(),
+			longitude: Joi.number().required(),
+			image: Joi.string(),
+			description: Joi.string(),
+			tourist_places: Joi.string(),
+			author: Joi.string().uuid().required(),
+		},
+	}),
+	citiesController.create,
+);
+
+citiesRouter.put(
+	'/:id',
+	celebrate({
+		[Segments.BODY]: {
+			name: Joi.string().required(),
+			state: Joi.string(),
+			country: Joi.string().required(),
+			population: Joi.number().required(),
+			latitude: Joi.number().required(),
+			longitude: Joi.number().required(),
+			image: Joi.string(),
+			description: Joi.string(),
+			tourist_places: Joi.string(),
+			author: Joi.string().uuid().required(),
+		},
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
+	citiesController.update,
+);
+
+citiesRouter.delete(
+	'/:id',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
+	citiesController.delete,
+);
+
+export default citiesRouter;
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,14 @@ import userRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import { Router } from 'express';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
+import citiesRouter from '@modules/cities/infra/http/routes/cities.routes';
 
 const routes = Router();
 
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/profile', profileRouter);
+
+routes.use('city', citiesRouter);
 
 export default routes;
