4522466c28f7443a992e387645b8c45c01648b11
kleberbarilli <kleberbarilli73@gmail.com>
2021-10-20 22:21:48 -0300

    add city-web

--- web/src/pages/NewCity/index.js
+++ web/src/pages/NewCity/index.js
@@ -1,115 +1,105 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect, useContext } from 'react';
 import './newcity.css';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 import { FaPlusCircle } from 'react-icons/fa';
 import Select from 'react-select';
 import countryList from 'react-select-country-list';
-import TagsInput from 'react-tagsinput'
-import Wizard from '../../components/Wizard';
+import TagsInput from 'react-tagsinput';
 import { FiUpload } from 'react-icons/fi';
-import avatar from '../../assets/avatar.png';
+import api from '../../services/api';
+import { AuthContext } from '../../contexts/auth';
+
 
 function NewCity() {
-	const [value, setValue] = useState('');
+	const [valueCountry, setValueCountry] = useState('');
 	const options = useMemo(() => countryList().getData(), []);
+	const { user } = useContext(AuthContext);
 
+	// Prop City
+	const [values, setValues] = useState({});
+	const [name, setName] = useState();
+	const [state, setState] = useState();
+	const [country, setCountry] = useState();
+	const [population, setPopulation] = useState();
+	const [latitude, setLatitude] = useState();
+	const [longitude, setLongitude] = useState();
+	const [description, setDescription] = useState();
 	const [tags, setTags] = useState([]);
-	const [imageCityUrl, setImageCityUrl]= useState();
+	const [imageCityUrl, setImageCityUrl] = useState();
 	const [imageCity, setImageCity] = useState();
+	const [imageCityName, setImageCityName] = useState();
 
-	function handleFile(e){
+	useEffect(() => {
+		//console.log(localStorage.getItem('SistemaUser'))
+
+
+		console.log(user.id)
+	  }, []);
+
+	function handleSubmit(e){
+		e.preventDefault();
+
+		if(name && valueCountry && population && latitude && longitude ){
+			handleAddCity()
+		}else{
+			alert("Preencha todos os campos obrigatórios")
+		}
+
+	}
+
+	async function handleAddCity(){
+		await api.post(`${process.env.REACT_APP_BASE_URL}/city`,{
+			name: name,
+			state: state,
+			country: valueCountry.label,
+			population: population,
+			latitude: latitude,
+			longitude: longitude,
+			description: description,
+			tourist_places: tags.toString(),
+			image: imageCity.name,
+			author: user.id
+		},{
+			headers: {
+				'Authorization': `Bearer ${user.token}`,
+				'Content-Type':'application/json'
+			}
+		}).then((res)=>{
+			alert('enviado')
+			console.log(res)
+		}).catch((err)=>{
+			console.log(err)
+		})
+	}
+
+
+	function handleFile(e) {
+		setImageCityName(e.target.files[0].name)
 		console.log(e.target.files[0]);
-		if(e.target.files[0]){
+		if (e.target.files[0]) {
 			const image = e.target.files[0];
 
-			if(image.type === 'image/jpeg' || image.type === 'image/png'){
+			if (image.type === 'image/jpeg' || image.type === 'image/png') {
 				setImageCity(image);
-				setImageCityUrl(URL.createObjectURL(e.target.files[0]))
-			}else {
-				alert("Envie uma imagem do tipo PNG OU JPEG")
+				setImageCityUrl(URL.createObjectURL(e.target.files[0]));
+			} else {
+				alert('Envie uma imagem do tipo PNG OU JPEG');
 				setImageCity(null);
 				return null;
 			}
 		}
 	}
 
-	const handleChange =(tags) => {
-		setTags(tags)
-	  }
-
+	const handleChange = tags => {
+		setTags(tags);
+	};
 
 	const changeHandler = value => {
-		setValue(value);
+		setValueCountry(value);
 	};
 
 
-
-	const Page1 = () => (
-		<div className="container">
-			<form className="form-profile">
-				<label>Nome da cidade</label>
-				<input type="text" name="city" />
-				<label>Estado</label>
-				<input type="text" name="state" />
-				<label>País</label>
-				<Select
-					className="select"
-					options={options}
-					value={value}
-					onChange={changeHandler}
-				/>
-			</form>
-		</div>
-	);
-
-	const Page2 = () => (
-		<div className="container">
-			<form className="form-profile">
-				<label>População</label>
-				<input type="number" name="population" />
-				<label>Latitude</label>
-				<input type="number" name="latitude" />
-				<label>Longitude</label>
-				<input type="number" name="longitude" />
-			</form>
-		</div>
-	);
-	const Page3 =()=> (
-
-		<div className="container">
-
-			<form className="form-profile">
-				<label>Descrição da cidade</label>
-				<span className="mini-texto">Fale um pouco sobre a cidade</span>
-				<textarea
-					rows="5"
-					cols="60"
-					placeholder="Fique a vontade para digitar sobre qualquer coisa legal"
-					name="population"
-				>
-					{' '}
-				</textarea>
-				<label style={{marginTop:15}}>Pontos Turísticos - Adicione abaixo, os principais lugares da cidade</label>
-				<TagsInput value={tags} onChange={handleChange} />
-
-				<label style={{marginTop:15}}>Adicione uma foto para representar a cidade</label>
-
-				<label className="foto-cidade
-						">
-							<span>
-								<FiUpload color="red" size={75} />
-							</span>
-							<input type="file" accept="image/*" onChange={handleFile} />
-							<br />
-							{imageCityUrl === null || undefined  ? <img src={avatar} width="250" alt="Foto da cidade"/> :
-							<img src={imageCityUrl} width="250" alt=""/>}
-						</label>
-						<button className="btn-save-city"  type="submit">Salvar</button>
-
-			</form>
-		</div>
-	)
 	return (
 		<div>
 			<Header />
@@ -119,11 +109,92 @@ function NewCity() {
 					<FaPlusCircle size={25} />
 				</Title>
 
-				<Wizard>
-					<Page1 />
-					<Page2 />
-					<Page3 />
-				</Wizard>
+				<button onClick={()=> console.log(name,state,population,longitude, tags, imageCityUrl, valueCountry.label)}> FSAASF</button>
+				<div className="container">
+					<form className="form-profile" onSubmit={handleSubmit}>
+						<label>Nome da cidade</label>
+						<input
+							type="text"
+							onChange={event => setName(event.target.value)}
+						/>
+						<label>Estado</label>
+						<input
+							type="text"
+							name="state"
+							onChange={event => setState(event.target.value)}
+						/>
+						<label>País</label>
+						<Select
+							className="select"
+							options={options}
+							value={valueCountry}
+							onChange={changeHandler}
+						/>
+
+						<label>População</label>
+						<input
+							type="number"
+							name="population"
+							onChange={event => setPopulation(event.target.value)}
+						/>
+						<label>Latitude</label>
+						<input
+							type="number"
+							name="latitude"
+							onChange={event => setLatitude(event.target.value)}
+						/>
+						<label>Longitude</label>
+						<input
+							type="number"
+							name="longitude"
+							onChange={event => setLongitude(event.target.value)}
+						/>
+
+						<label>Descrição da cidade</label>
+						<span className="mini-texto">
+							Fale um pouco sobre a cidade
+						</span>
+						<textarea
+							rows="5"
+							cols="60"
+							placeholder="Fique a vontade para digitar sobre qualquer coisa legal"
+							name="description"
+							onChange={event => setDescription(event.target.value)}
+						>
+							{' '}
+						</textarea>
+						<label style={{ marginTop: 15 }}>
+							Pontos Turísticos - Adicione abaixo, os principais
+							lugares da cidade
+						</label>
+						<TagsInput value={tags} onChange={handleChange} />
+
+						<label style={{ marginTop: 15 }}>
+							Adicione uma foto para representar a cidade
+						</label>
+
+						<label
+							className="foto-cidade
+						"
+						>
+							<span>
+								<FiUpload color="#4287f5" size={50} />
+							</span>
+							<input
+								type="file"
+								accept="image/*"
+								onChange={handleFile}
+							/>
+							<span className="city-name">{imageCityName}</span>
+							<br />
+
+						</label>
+						<button  className="btn-save-city" type="submit">
+							Salvar
+						</button>
+
+					</form>
+				</div>
 			</div>
 		</div>
 	);
