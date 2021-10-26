222afa45574b84ba691996ff996ee84d6bdd8abc
kleberbarilli <kleberbarilli73@gmail.com>
2021-10-26 09:32:20 -0300

    paginacao mostrar mais

--- web/src/pages/Cities/index.js
+++ web/src/pages/Cities/index.js
@@ -1,40 +1,46 @@
 import { useState, useEffect } from 'react';
+import './cities.css';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { FaCity, FaPlus, FaSearchPlus, FaEdit } from 'react-icons/fa'
+import { FaCity, FaPlus, FaSearchPlus, FaEdit } from 'react-icons/fa';
 import api from '../../services/api';
-import { format } from 'date-fns';
+import { PageActions } from '../../components/Pageaction';
 
 function Cities() {
+	const [page, setPage] = useState(1);
+	const [totalPage, setTotalPage] = useState();
+
 	const [cities, setCities] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const [loadingMore, setLoadingMore] = useState(false);
 	const [isEmpty, setIsEmpty] = useState(false);
-	const [lastDocs, setLastDocs] = useState();
+
 
 	useEffect(() => {
-
 		loadCities();
-		return () => {
-
-		}
-	}, [])
+	}, [page]);
 
 	async function loadCities() {
-		await api.get(`/city`,
-			{})
-			.then((res) => {
-				//console.log(res.data.data)
-				let a = Object.values(res.data.data)
-				console.log(a[0].name)
-				updateState(a)
+		await api
+			.get(`/city?page=${page}`, {
+				params: {
+					per_page: 5,
+				},
 			})
-			.catch((err) => {
-				console.log(err)
+			.then(res => {
+				let a = Object.values(res.data.data);
+				updateState(a);
+			})
+			.catch(err => {
+				console.log(err);
 				setLoadingMore(false);
-			})
+			});
 		setLoading(false);
+	}
+
+	function handlePage(action) {
+		setPage(action === 'back' ? page - 1 : page + 1);
 	}
 
 	async function updateState(res) {
@@ -43,7 +49,7 @@ function Cities() {
 		if (!isDataEmpty) {
 			let list = [];
 
-			res.forEach((data) => {
+			res.forEach(data => {
 				list.push({
 					id: data.id,
 					name: data.name,
@@ -52,35 +58,15 @@ function Cities() {
 					population: data.population,
 					created_at: data.created_at,
 					updated_at: data.updated_at,
+				});
+			});
 
-				})
-			})
-
-			setCities(cities => [...cities, ...list])
-
+			setCities(cities => [...cities, ...list]);
 		} else {
 			setIsEmpty(true);
 		}
 
 		setLoadingMore(false);
-	}
-
-	if (loading) {
-		return (
-			<div>
-				<Header />
-
-				<div className="content">
-					<Title name="Cidades">
-						<FaCity size={25} />
-					</Title>
-
-					<div className="container dashboard">
-						<span>Buscando cidades.....</span>
-					</div>
-				</div>
-			</div>
-		)
 	}
 
 	return (
@@ -91,7 +77,6 @@ function Cities() {
 				<Title name="Cidades">
 					<FaCity size={25} />
 				</Title>
-
 				{cities.length === 0 ? (
 					<div className="container dashboard">
 						<span>Nenhuma cidade registrada...</span>
@@ -122,31 +107,64 @@ function Cities() {
 								{cities.map((item, index) => {
 									return (
 										<tr key={index}>
-											<td data-label="Cidade">{item.name}</td>
-											<td data-label="Estado">{item.state}</td>
-											<td data-label="País">{item.country}</td>
-											<td data-label="População">{item.population}</td>
-											<td data-label="Cadastrado">{item.created_at}</td>
+											<td data-label="Cidade">
+												{item.name}
+											</td>
+											<td data-label="Estado">
+												{item.state}
+											</td>
+											<td data-label="País">
+												{item.country}
+											</td>
+											<td data-label="População">
+												{item.population}
+											</td>
+											<td data-label="Cadastrado">
+												{item.created_at}
+											</td>
 											<td data-label="#">
-												<button className="action" style={{ backgroundColor: '#3583f6' }}>
-													<FaSearchPlus color="#000" size={17} />
+												<button
+													className="action"
+													style={{
+														backgroundColor:
+															'#3583f6',
+													}}
+												>
+													<FaSearchPlus
+														color="#000"
+														size={17}
+													/>
 												</button>
-												<Link to={`/newcity/${item.id}`} className="action" style={{ backgroundColor: '#f7902f' }}>
-													<FaEdit color="#000" size={17} />
+												<Link
+													to={`/newcity/${item.id}`}
+													className="action"
+													style={{
+														backgroundColor:
+															'#f7902f',
+													}}
+												>
+													<FaEdit
+														color="#000"
+														size={17}
+													/>
 												</Link>
-
 											</td>
 										</tr>
-									)
+									);
 								})}
-
 							</tbody>
 						</table>
-
+						<PageActions>
+							<button
+								type="button"
+								onClick={() => handlePage('next')}
+								style={{display:'flex', justifyContent:'center', alignItems:'center'}}
+							>
+								Mostrar mais
+							</button>
+						</PageActions>
 					</>
 				)}
-
-
 			</div>
 		</div>
 	);
