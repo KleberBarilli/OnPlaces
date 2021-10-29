a5c2ebca3ba4d0aa9439d5c560c541ca48279ab1
kleberbarilli <kleberbarilli73@gmail.com>
2021-10-29 18:47:45 -0300

    city map

--- web/src/pages/City/index.js
+++ web/src/pages/City/index.js
@@ -10,6 +10,7 @@ import { AuthContext } from '../../contexts/auth';
 import { MdOutlinePeopleAlt } from 'react-icons/md';
 import ReactCountryFlag from 'react-country-flag';
 import lookup from 'country-code-lookup';
+import CityMap from '../../components/City/Map';
 
 function City() {
 	const { id } = useParams();
@@ -76,7 +77,21 @@ function City() {
 						/>
 					</Title>
 				</div>
-				<div className="description">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</div>
+				<div className="description" >
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</div>
+				{city.length === 0 ? (
+					<div>Carregando Mapa....</div>
+				) : (
+					<div style={{display:'flex', justifyContent:'center'}}>
+						<CityMap width={750} city={city} />
+					</div>
+
+				)}
+				<div className="description" >
+						It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+					</div>
+
 				<div className="container">
 					<img
 						className="city-image"
