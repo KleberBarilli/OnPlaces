259af8c16f3471994ebf825f4a02185740805d1a
kleber.barilli <kleber.barilli73@gmail.com>
2022-03-31 17:42:49 -0300

    heroku test: redis

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -10,9 +10,7 @@ interface ICacheConfig {
 export default {
 	config: {
 		redis: {
-			host: 'ec2-3-226-160-187.compute-1.amazonaws.com',
-			port: 19360,
-			pass: 'p995d9de119249c11693bd09f8b1c3df090e6af8fd4afbb3de414cbda5f839b7f',
+			url: process.env.REDIS_TLS_URL,
 		},
 	},
 	driver: 'redis',
