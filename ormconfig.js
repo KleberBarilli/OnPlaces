52ddee5aee13cbff6db24075408acdb98a92350d
kleber.barilli <kleber.barilli73@gmail.com>
2022-04-04 13:39:39 -0300

    .

--- ormconfig.js
+++ ormconfig.js
@@ -1,18 +1,16 @@
-let ext = process.env.NODE_ENV === 'development' ? "ts" : "js";
-let path = process.env.NODE_ENV === 'development' ? "src" : "dist";
+let ext = process.env.NODE_ENV === 'development' ? 'ts' : 'js';
+let path = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
 
 module.exports = {
-	"type": "postgres",
-	"url":`${process.env.DATABASE_URL}`,
-	"ssl": {rejectUnauthorized: false }, // desativar no local.
-	"synchronize": false,
-	"migrationsRun": false,
-	"logging": false,
-	"entities": [`./${path}/modules/**/typeorm/entities/*.${ext}`],
-	"migrations": [
-		`./${path}/shared/infra/typeorm/migrations/*.${ext}`
-	],
-	"cli": {
-		"migrationsDir": "./src/shared/infra/typeorm/migrations"
-	}
- }
+	type: 'postgres',
+	url: `${process.env.DATABASE_URL}`,
+	ssl: { rejectUnauthorized: false }, // desativar no local.
+	synchronize: false,
+	migrationsRun: false,
+	logging: false,
+	entities: [`./${path}/modules/**/typeorm/entities/*.${ext}`],
+	migrations: [`./${path}/shared/infra/typeorm/migrations/*.${ext}`],
+	cli: {
+		migrationsDir: './src/shared/infra/typeorm/migrations',
+	},
+};
